6bcd0cb005e22aa75dc021890b280cacee132f21
Nikki Gyawali <nikkizzz70@gmail.com>
2025-09-08 14:22:51 +0545

    Initail project upload

---
+++ Sikshya-Ghar/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Footer from './Components/Footer'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
